44302e696db7ffef6439f1565df3386aa3939331
Vadim <vem23@tpu.ru>
2025-12-16 07:07:17 -0800

    MainPage

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './main.css'
-import { AppContainer } from "./AppContainer";
+import  App  from "./App.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppContainer />
+    <App />
   </StrictMode>,
 )
