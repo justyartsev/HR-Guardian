a45dfa90e79a80e69ed9af7326d3b50861c7bdc4
Olekkm <138139365+Olekkm@users.noreply.github.com>
2025-12-18 17:45:23 +0700

    Revert "Merge branch 'deploy' into Frontend"
    This reverts commit 784b82050498d55bcf430153b3fd0735f0e0f80e, reversing
    changes made to 5dc0f7f56b99d9d486af5c5ef8daf038d3c9b0af.

--- Backend/core/jwt.py
+++
@@ -1,9 +0,0 @@
-from jose import jwt
-from datetime import datetime, timedelta
-from .config import settings
-
-def create_token(data: dict):
-    to_encode = data.copy()
-    expire = datetime.now() + timedelta(hours=24)
-    to_encode["exp"] = expire
-    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
