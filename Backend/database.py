045ddc813557e0313fbffe4a066c4943cab0e2b0
kiznaiver <kiznaiver.kk@gmail.com>
2025-12-21 11:28:35 +0700

    UTF-8 кодировка для Postgres

--- Backend/database.py
+++ Backend/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
@@ -8,6 +8,14 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
+
+# При каждом подключении явно устанавливаем UTF-8
+@event.listens_for(engine, "connect")
+def receive_connect(dbapi_conn, connection_record):
+    with dbapi_conn.cursor() as cur:
+        cur.execute("SET client_encoding TO 'utf8'")
+    dbapi_conn.commit()
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
